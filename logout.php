e24a84daedae1c78964a023a2248fe64aa9526fa
Sean Chickosky <chickosky@gmail.com>
2016-12-05 21:45:10 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require("common.php");
-    // Remove the user date from the session
-    unset($_SESSION['user']);
-
-    // Redirect to login
-    header("Location: login.php");
-    die("Redirecting to: login.php");
